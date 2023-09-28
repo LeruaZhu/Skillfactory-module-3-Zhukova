013217dd1aca575294071ace24afb56528f5ede2
LeruaZhu <134527496+LeruaZhu@users.noreply.github.com>
2023-09-28 15:19:53 +0300

    Add files via upload

---
+++ My first app/My first app/GroupWithFolder)/TestFile.swift
@@ -0,0 +1,8 @@
+//
+//  TestFile.swift
+//  My first app
+//
+//  Created by Жукова Валерия on 25.05.2023.
+//
+
+import Foundation
---
+++ My first app/My first app/My_first_appApp.swift
@@ -0,0 +1,26 @@
+//
+//  My_first_appApp.swift
+//  My first app
+//
+//  Created by Жукова Валерия on 24.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct My_first_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+       
+        }
+ 
+    }
+
+    
+
+    
+        
+    
+
